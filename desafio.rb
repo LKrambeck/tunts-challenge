755d3173f436fa76b6e1074bca6eb31cd26c4b29
Krambeck <leonardokrambeck@gmail.com>
2021-03-15 20:29:01 -0300

    Implementa metodo run, integra com api google docs e traduz entrada

--- desafio.rb
+++ desafio.rb
@@ -2,7 +2,7 @@ class Student
 
     attr_accessor :registry, :missedClasses, :grade1, :grade2, :grade3, :gradeAverage, :situation, :examGrade
 
-    def getStudent (row)
+    def buildStudent (row)
         @registry = row[0].to_i
         @missedClasses = row[2].to_i
         @grade1 = row[3].to_i
@@ -14,7 +14,36 @@ end
 
 class GradesProcessor
 
-    def attendance? (student)
+    def run (student)
+
+        if hasMinimumAttendance?(student)
+
+            gradeAverage(student)
+        
+            if student.gradeAverage >= 70
+                student.situation = "Aprovado"
+                student.examGrade = 0
+        
+            elsif student.gradeAverage < 50
+                student.situation = "Reprovado"
+                student.examGrade = 0
+                
+            else
+                student.situation = "Exame Final"
+                student.examGrade = necessaryGrade(student)
+            end
+        
+        else
+        
+            student.situation = "Reprovado por Falta"
+            student.examGrade = 0
+        
+        end
+        puts student.inspect
+    end        
+
+    private 
+    def hasMinimumAttendance? (student)
         student.missedClasses <= 15
     end
 
@@ -27,45 +56,3 @@ class GradesProcessor
     end
 
 end
-
-student = Student.new
-processor = GradesProcessor.new
-
-require 'csv'
-table = CSV.read("teste.csv")
-
-# 0 = registry
-# 2 = missedClasses
-# 3 = grade1
-# 4 = grade2
-# 5 = grade3 
-# 6 = situation
-# 7 = examGrade
-
-student.getStudent (table[0])
-
-if processor.attendance?(student)
-
-    processor.gradeAverage(student)
-
-    if student.gradeAverage >= 70
-        student.situation = "Aprovado"
-        student.examGrade = 0
-
-    elsif student.gradeAverage < 50
-        student.situation = "Reprovado"
-        student.examGrade = 0
-        
-    else
-        student.situation = "Exame Final"
-        student.examGrade = processor.necessaryGrade(student)
-    end
-
-else
-
-    student.situation = "Reprovado por Falta"
-    student.examGrade = 0
-
-end
-
-puts "#{student.registry},#{student.missedClasses},#{student.grade1},#{student.grade2},#{student.grade3},#{student.gradeAverage},#{student.situation},#{student.examGrade}"
