bd08f288af8dc285e6886983e8f5c1c74e4d66c5
Krambeck <leonardokrambeck@gmail.com>
2021-03-16 12:08:20 -0300

    muda o nome de desafio.rb para challenge.rb e adiciona metodo para logs

--- challenge.rb
+++ challenge.rb
@@ -9,6 +9,7 @@ class Student
         @grade1 = row[3].to_i
         @grade2 = row[4].to_i
         @grade3 = row[5].to_i
+        puts "Student: #{row[1]}"
     end
 end
 
@@ -47,8 +48,14 @@ class GradesProcessor
     end
 end
 
-def execute
+def print_logs (student)
+    puts "Attendance: #{60-student.missedClasses}/60 (#{(60-student.missedClasses)*100/60}%)"
+    puts "Grade1: #{student.grade1}, Grade2: #{student.grade2}, Grade3: #{student.grade3}"
+    puts "Average grade: #{student.grade_average}"
+    puts "Situation: #{student.situation}"
+end
 
+def execute
     student = Student.new
     grades_processor = GradesProcessor.new
     
@@ -57,12 +64,18 @@ def execute
     
     spreadsheet_id = "1a2n_Ej9-xJUOfWTY-Z8sU_RP2v5EP03K71Yj-cdo4q8"
     
-    response = cloud_service.read_spreadsheet(spreadsheet_id)
+    puts "Reading spreadsheet..."
+    if (response = cloud_service.read_spreadsheet(spreadsheet_id))
+        puts "Done."
+    else
+        puts "Cannot read spreadsheet."
+    end
     
     response.values.each_with_index do |row,index|
       range_name = "engenharia_de_software!G#{4+index}:H"
       student.build_student(row)
       grades_processor.calculate_grades(student)
+      print_logs (student)
       cloud_service.write_spreadsheet(student,range_name, spreadsheet_id)
     end
 end
